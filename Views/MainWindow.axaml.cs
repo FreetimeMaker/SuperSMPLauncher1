a49502f7b1a00a2477ddeb62704dd7d31348017b
Freetime Maker <jamieachatz@gmail.com>
2025-12-14 21:11:55 +0100

    wow

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SuperSMPLauncher.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
